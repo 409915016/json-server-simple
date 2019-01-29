cebd5e1214da811c85223a1500c7d62fd7481a3f
mather <misaka@misaka.im>
2019-01-29 17:30:02 +0800

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+//const middlewares = jsonServer.defaults()
+let middlewares = require('./middlewares')
+
+server.use((req, res, next) => {
+  next()
+})
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
